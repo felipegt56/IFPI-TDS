22f11b8a0999bfb8ef3d029cb05d95c95013d328
João Felipe <69603279+felipegt56@users.noreply.github.com>
2021-05-04 21:30:56 -0300

    commit

--- 2021.1/Back_End/Projeto BLX/src/infra/sqlalchemy/models/models.py
+++ 2021.1/Back_End/Projeto BLX/src/infra/sqlalchemy/models/models.py
@@ -1,5 +1,16 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from src.infra.sqlalchemy.config.database import Base
+
+class Usuario(Base):
+    __tablename__ = 'usuario'
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String)
+    senha = Column(String)
+    telefone = Column(String)
+
+    produtos = relationship('Produto', back_populates='usuario')
 
 
 class Produto(Base):
@@ -10,3 +21,7 @@ class Produto(Base):
     preco = Column(Float)
     detalhes = Column(String)
     disponivel = Column(Boolean)
+    tamanhos = Column(String)
+    id_usuario = Column(Integer, ForeignKey('usuario.id', name='fk_usuario' ))
+
+    usuario = relationship('Usuario', back_populates='produtos')
--- 2021.1/Back_End/Projeto BLX/src/schema/schemas.py
+++ 2021.1/Back_End/Projeto BLX/src/schema/schemas.py
@@ -1,21 +1,46 @@
 from pydantic import BaseModel
 from typing import Optional, List
 
-class Usuario(BaseModel):
-    id: Optional[str] = None
+class Produtosimples(BaseModel):
+    id: Optional[int] = None
     nome: str
-    telefone: str
-
-class Produto(BaseModel):
-    id: Optional[str] = None
-    nome: str
-    detalhes: str
     preco: float
-    disponivel: bool = False
+    disponivel: bool
 
     class Config:
         orm_mode = True
 
+class Usuario(BaseModel):
+    id: Optional[int] = None
+    nome: str
+    telefone: str
+    senha: str
+    produtos: List[Produtosimples] = []
+
+    class Config:
+        orm_mode = True
+
+
+class UsuarioSimples(BaseModel):
+    id: Optional[int] = None
+    nome: str
+    telefone: str
+
+    class Config:
+        orm_mode = True
+
+
+class Produto(BaseModel):
+    id: Optional[int] = None
+    nome: str
+    detalhes: str
+    preco: float
+    disponivel: bool = False
+    usuario_id: Optional[int]
+    usuario: Optional[UsuarioSimples]
+
+    class Config:
+        orm_mode = True
 
 class Pedido(BaseModel):
     id: Optional[str] = None
