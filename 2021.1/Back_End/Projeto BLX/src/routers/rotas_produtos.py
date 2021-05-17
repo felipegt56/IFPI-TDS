b47b3b59654fe48df978edccab853d4f42934eee
João Felipe <69603279+felipegt56@users.noreply.github.com>
2021-05-16 22:58:44 -0300

    Nova rota criada

--- 2021.1/Back_End/Projeto BLX/src/routers/rotas_produtos.py
+++ 2021.1/Back_End/Projeto BLX/src/routers/rotas_produtos.py
@@ -1,30 +1,37 @@
 from fastapi import APIRouter, status, Depends, HTTPException
 from typing import List
 from sqlalchemy.orm import Session
-from src.schema.schemas import Produto, ProdutoSimples
+from src.schema.schemas import Produto, Produtosimples
 from src.infra.sqlalchemy.config.database import get_db
 from src.infra.sqlalchemy.repositorios.repositorio_produto \
     import RepositorioProduto
 
 router = APIRouter()
 
-@round.post('/produtos', status_code=status.HTTP_201_CREATED, response_model=Produtosimples)
+@router.post('/produtos', status_code=status.HTTP_201_CREATED, response_model=Produtosimples)
 def criar_produto(produto: Produto, db: Session = Depends(get_db)):
     produto_criado = RepositorioProduto(db).criar(produto)
     return produto_criado
 
-@round.get('/produtos', response_model=List[Produto])
+@router.get('/produtos', response_model=List[Produto])
 def listar_produtos(db: Session = Depends(get_db)):
     produtos = RepositorioProduto(db).listar()
     return produtos
 
-@round.put('/produtos/{id}', response_model=Produtosimples)
+@router.get('/produtos/{id}')
+def exibir_produto(id: int, session: Session = Depends(get_db)):
+    produto_localizado = RepositorioProduto(session).buscarPorId(id)
+    if not produto_localizado:
+        raise HTTPException(status_code=404, detail='Produto não Localizado')
+    return produto_localizado
+
+@router.put('/produtos/{id}', response_model=Produtosimples)
 def atualizar_produto(id: int, produto: Produto, session: Session = Depends(get_db)):
     RepositorioProduto(session).editar(id, produto)
     produto.id = id
     return produto
 
-@round.delete('/produtos/{id}')
+@router.delete('/produtos/{id}')
 def remover_produto(id: int, session: Session = Depends(get_db)):
     RepositorioProduto(session).remover(id)
     return
