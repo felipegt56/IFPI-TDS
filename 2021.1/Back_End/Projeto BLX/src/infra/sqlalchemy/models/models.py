83d744517228c874618e493aca6f75750ab66609
João Felipe <69603279+felipegt56@users.noreply.github.com>
2021-05-20 16:07:58 -0300

    Modelo Pedido criado

--- 2021.1/Back_End/Projeto BLX/src/infra/sqlalchemy/models/models.py
+++ 2021.1/Back_End/Projeto BLX/src/infra/sqlalchemy/models/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, relationships
 from src.infra.sqlalchemy.config.database import Base
 
 class Usuario(Base):
@@ -10,8 +10,8 @@ class Usuario(Base):
     senha = Column(String)
     telefone = Column(String)
 
-    produtos = relationship('Produto', back_populates='usuario')
-
+    produtos = relationship('Produtos', back_populates='usuario')
+    pedidos = relationship('Pedidos', back_populates='usuario')
 
 class Produto(Base):
     __tablename__ = 'produto'
@@ -24,4 +24,18 @@ class Produto(Base):
     tamanhos = Column(String)
     id_usuario = Column(Integer, ForeignKey('usuario.id', name='fk_usuario' ))
 
-    usuario = relationship('Usuario', back_populates='produtos')
+    usuario = relationship('Usuarios', back_populates='produtos')
+
+class Pedido(Base):
+    __tablename__ = 'pedido'
+
+    id = Column(Integer, primary_key=True, index=True)
+    quantidade = Column(Integer)
+    local_entrega = Column(String)
+    opcao = Column(String)
+    observacoes = Column(String)
+    id_produto = Column(Integer, ForeignKey('produto.id', name='fk_pedido_produto' ))
+    id_usuario = Column(Integer, ForeignKey('usuario.id', name='fk_pedido_usuario' ))
+
+    usuario = relationship('Usuarios', back_populates='pedidos')
+    produto = relationship('Produtos')
