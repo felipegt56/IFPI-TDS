512b57f093a09c5a56091c5b839b8da7f14597cf
João Felipe <69603279+felipegt56@users.noreply.github.com>
2021-05-20 21:41:26 -0300

    Modelos pedido

--- 2021.1/Back_End/Projeto BLX/src/infra/sqlalchemy/models/models.py
+++ 2021.1/Back_End/Projeto BLX/src/infra/sqlalchemy/models/models.py
@@ -1,41 +1,45 @@
 from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
-from sqlalchemy.orm import relationship, relationships
+from sqlalchemy.orm import relationship
 from src.infra.sqlalchemy.config.database import Base
+
 
 class Usuario(Base):
     __tablename__ = 'usuario'
 
     id = Column(Integer, primary_key=True, index=True)
     nome = Column(String)
-    senha = Column(String)
     telefone = Column(String)
+    senha = Column(String)
 
-    produtos = relationship('Produtos', back_populates='usuario')
-    pedidos = relationship('Pedidos', back_populates='usuario')
+    produtos = relationship('Produto', back_populates='usuario')
+    pedidos = relationship('Pedido', back_populates='usuario')
+
 
 class Produto(Base):
     __tablename__ = 'produto'
 
     id = Column(Integer, primary_key=True, index=True)
     nome = Column(String)
-    preco = Column(Float)
     detalhes = Column(String)
+    preco = Column(Float)
     disponivel = Column(Boolean)
     tamanhos = Column(String)
-    id_usuario = Column(Integer, ForeignKey('usuario.id', name='fk_usuario' ))
+    usuario_id = Column(Integer, ForeignKey('usuario.id', name='fk_usuario'))
 
-    usuario = relationship('Usuarios', back_populates='produtos')
+    usuario = relationship('Usuario', back_populates='produtos')
+
 
 class Pedido(Base):
     __tablename__ = 'pedido'
 
     id = Column(Integer, primary_key=True, index=True)
     quantidade = Column(Integer)
-    local_entrega = Column(String)
-    opcao = Column(String)
-    observacoes = Column(String)
-    id_produto = Column(Integer, ForeignKey('produto.id', name='fk_pedido_produto' ))
-    id_usuario = Column(Integer, ForeignKey('usuario.id', name='fk_pedido_usuario' ))
+    local_entrega  = Column(String)
+    tipo_entrega = Column(String)
+    observacao = Column(String)
 
-    usuario = relationship('Usuarios', back_populates='pedidos')
-    produto = relationship('Produtos')
+    usuario_id = Column(Integer, ForeignKey('usuario.id', name='fk_pedido_usuario'))
+    produto_id = Column(Integer, ForeignKey('produto.id', name='fk_pedido_produto'))
+
+    usuario = relationship('Usuario', back_populates='pedidos')
+    produto = relationship('Produto')
