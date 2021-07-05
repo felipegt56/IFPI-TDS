d490ec2158882fac2d64dfac6ea86c5dbdb0117c
João Felipe <69603279+felipegt56@users.noreply.github.com>
2021-07-04 22:48:04 -0300

    Arquivo main.dart

--- 2021.1/Prog_Dispositivos_Moveis/Projetos/Semana_13/google_maps_in_flutter/lib/main.dart
+++ 2021.1/Prog_Dispositivos_Moveis/Projetos/Semana_13/google_maps_in_flutter/lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -11,7 +12,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  final LatLng _center = const LatLng(46.5214567, -122.677433);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
